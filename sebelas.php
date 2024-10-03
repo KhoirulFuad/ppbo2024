a100ec7090a55265b5ca73221666c82abd37ba4c
Khoirul Fuad <khoirulfuads101@gmail.com>
2024-10-03 22:47:38 +0700

    Praktikum 5F - Membuat Objek Pewarisan

--- sebelas.php
+++ sebelas.php
@@ -1,21 +1,8 @@
 <?php
 
+require_once 'vendor/autoload.php';
+
 use App\Admin\Dosen;
 
-$dian = new Dosen(
-    198411132015041001,     
-    "Dian Prawira",         
-    62111111,               
-    "Jln Purnama",          
-    "0013118405"            
-);
-
-echo "Nama Dosen: " . $dian->nama . "\n";          
-echo "NIP: " . $dian->nip . "\n";                 
-echo "NIDN: " . $dian->nidn . "\n";               
-echo "Nomor HP: " . $dian->getNoHP() . "\n";      
-echo "Alamat: " . $dian->alamat . "\n";           
-
-$dian->mengajar(); 
-
-?>
+$dian = new Dosen(198411132015041001, "Dian Prawira", "Jln Purnama", 62111111, "0013118405");
+$dian->mengajar();
